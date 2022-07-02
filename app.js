65dfeeeefa9021ca2d046c64a52841bf7f825c93
Joel.BH.Zhang <rugoingdown@gmail.com>
2022-07-02 13:05:40 +0800

    add saml

--- app.js
+++ app.js
@@ -3,6 +3,36 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport');
+var session = require('express-session');
+var fs = require('fs');
+
+var SamlStrategy = require('passport-saml').Strategy;
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
+passport.use(new SamlStrategy(
+  {
+    path: '/pdms-saml',
+    entryPoint: 'https://login.microsoftonline.com/f074dbeb-1cc3-4857-a3ba-17253091f5d3/saml2',
+    issuer: 'pdms-saml',
+    cert: fs.readFileSync('PDMS-SAML.cer', 'utf-8'),
+    signatureAlgorithm: 'sha256'
+  },
+  function (profile, done) {
+    return done(null,
+      {
+        id: profile['nameID'],
+        email: profile['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress'],
+        displayName: profile['http://schemas.microsoft.com/identity/claims/displayname'],
+        firstName: profile['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname'],
+        lastName: profile['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname']
+      });
+  })
+);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -17,18 +47,43 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session(
+  {
+    resave: true,
+    saveUninitialized: true,
+    secret: 'this shit hits'
+  }));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.get('/login',
+  passport.authenticate('saml', {
+    successRedirect: '/',
+    failureRedirect: '/login'
+  })
+);
+app.post('/pdms-saml',
+  passport.authenticate('saml', {
+    failureRedirect: '/',
+    failureFlash: true
+  }),
+  function (req, res) {
+    console.log(req);
+    res.redirect('/');
+  }
+);
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
